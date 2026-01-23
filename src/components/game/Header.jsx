6f284456801617eeeb89d65efeafb07381134bdf
Zaara-Fatima <bano6096@gmail.com>
2026-01-23 17:29:29 +0530

    Refactor Header component layout and styling

--- src/components/game/Header.jsx
+++ src/components/game/Header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export const Header = ({onClick}) => {
+export const Header = ({onClick, className}) => {
   const navigate= useNavigate();
   return (
-    <div className="flex flex-row justify-between items-center top-[20px] absolute w-full md:px-20">
-      <div className="arya-regular md:text-[40px] text-[30px] text-[white] relative md:left-[500px] left-[70px]">
+    <div className={` grid grid-cols-2  w-full ${className}`}>
+      <div className="arya-regular  text-[min(8vw,2.5rem)] text-[white] justify-self-end">
         PUZZLE GAME
       </div>
-      <div className="hamburger flex flex-col justify-between md:h-4 md:w-6 h-3 w-5 gap-0 absolute left-[20px] md:static" onClick={onClick}>
+      <div className="justify-self-end pr-[10%]">
+      <div className="hamburger flex flex-col justify-between  h-[2vh] w-[4vh] gap-0 " onClick={onClick}>
           <div className="bg-white p-0 m-0 h-[2px]"></div>
           <div className="bg-white p-0 m-0 h-[2px]"></div>
           <div className="bg-white p-0 m-0 h-[2px]"></div>
+      </div>
       </div>
     </div>
   );
