c3416dd6c315701e261618775eb966e2274ccf71
Zaara-Fatima <bano6096@gmail.com>
2026-01-23 17:38:36 +0530

    Refactor Upload component layout and styles

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -12,7 +12,7 @@ export const Upload = () => {
   return (
     <div>
       <div
-        className="flex flex-col items-center justify-center relative h-screen  bg-[#7DC383] text-center"
+        className=" grid md:grid-cols-3 grid-rows-2  w-full h-[100vh] bg-[#7DC383] text-center"
         style={{
           backgroundColor: "#7dc383",
           backgroundImage:
@@ -20,57 +20,68 @@ export const Upload = () => {
           backgroundRepeat: "repeat",
         }}
       >
-        <Header onClick={() => navigate("/setting")} />
-        <div className="flex md:flex-row flex-col justify-center items-center absolute md:left-[445px] left-[190px] md:top-[300px] top-[160px]">
+        <Header onClick={() => navigate("/setting")} className="fixed top-[5%]"/>
+        <div className="md:col-span-2 ">
+        <div className="grid place-items-center w-full h-[100vh]">
+          <PuzzlePreview className="bg-[#DCCE99] md:w-[55vw] w-[70vw]  md:h-[50vh]  h-[30vh]  rounded-xl md:mt-[3rem] shadow-[inset_2px_2px_2px_rgba(0,0,0,0.3)] "  />
+          <div className="xl:fixed md:fixed md:bottom-[20em] z-10 flex flex-col relative bottom-[20rem] items-center justify-center gap-4 ">
           <Icon
-            className="  md:w-[25px] md:h-[25px] w-[10px] h-[10px]  z-10 text-white"
+            className=" text-white"
             svg={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
-                className="md:size-20 size-10"
+                className="size-15"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
                 />
               </svg>
             }
           />
-          <h6 className="z-10 text-white absolute md:top-[100px] top-[50px] w-[450px] md:text-[15px] text-[10px] font-bold ml-10">
-            Upload your image (JPG,PNG) max-size:10mb
+          <h6 className=" text-white font-bold text-[min(4rem,0.7rem)]  text-center">
+            Upload your image (JPG,PNG)<br/> max-size:10mb
           </h6>
+          </div>
+          
         </div>
-
-        <PuzzlePreview className="bg-[#DCCE99] md:w-[750px] w-[350px] md:h-[450px] h-[150px] rounded-xl absolute md:left-[100px] left-[40px] top-[125px] shadow-[inset_2px_2px_2px_rgba(0,0,0,0.3)]" />
+        </div>
+        <div className="flex flex-col flex-wrap justify-center relative items-center md:row-span-2">
         <SelectPuzzlePiece
-          className="md:right-[75px] absolute right-[40px] md:top-[127px] top-[350px] "
+          className="md:w-[30vw] w-[70vw] md:h-[50vh] h-[45vh] fixed md:mt-[3rem] mt-[-3rem] md:mr-[2rem]"
           text="Select Puzzle"
-          className1="md:w-[400px] md:h-[450px] w-[350px] h-[260px]"
+          
         />
-        <div className="text-white font-bold text-[18px] md:top-[260px] top-[410px] md:right-[380px] right-[300px] absolute z-10">Default:</div>
-        <div className=" absolute z-10 md:top-[300px] top-[450px] md:right-[330px] right-[250px]">
+        <div className="flex fixed md:flex-col justify-center items-center md:mt-[4rem] mt-[-8rem] md:mr-[2rem] gap-[2rem] z-10 w-">
+        
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-white font-bold text-[18px] ">Default:</div>
           <PuzzleImgPrev
             src={defaultImages[1]}
-            className1="w-[100px] h-[100px] shadow-3xl ring-2 ring-black/20"
-            className2="rounded-2xl  bg-[#DCCE99]  flex justify-center items-center "
+            className1="md:w-[8vw] max-w-[15vw] h-[10vh] shadow-3xl ring-2 ring-black/20"
+            className2=""
             onClick={() => navigate("/select")}
           />
         </div>
-        <div className="text-white font-bold text-[18px] md:top-[260px] top-[410px]  md:right-[155px] right-[100px] absolute z-10">Uploaded:</div>
-        <div className=" absolute z-10 md:top-[300px] top-[450px] md:right-[110px] right-[90px] ">
+        
+        <div className="flex flex-col justify-center items-center ">
+          <div className="text-white font-bold text-[18px] ">Uploaded:</div>
           <PuzzleImgPrev
             src={defaultImages[1]}
-            className1="w-[100px] h-[100px] ring-2 ring-black/20 "
-            className2="rounded-2xl  bg-[#DCCE99] flex justify-center items-center"
+            className1="md:w-[8vw] max-w-[15vw] h-[10vh] ring-2 ring-black/20 "
+            className2=""
             onClick={() => navigate("/select")}
           />
         </div>
+      </div> 
+      </div>
       </div>
     </div>
+   
   );
 };
