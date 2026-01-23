8497da86e88e9ababcd9a035c3ad8ae252f92746
Zaara-Fatima <bano6096@gmail.com>
2026-01-23 17:37:51 +0530

    Refactor SelectPuzzle component for improved layout
    Refactor SelectPuzzle component layout and styling.

--- src/pages/SelectPuzzle.jsx
+++ src/pages/SelectPuzzle.jsx
@@ -12,7 +12,7 @@ export const SelectPuzzle = () => {
   return (
     <div>
       <div
-        className="flex flex-col items-center justify-center h-screen  bg-[#7DC383] text-center"
+        className="grid grid-rows-1 place-items-center-safe h-screen  bg-[#7DC383] text-center"
         style={{
           backgroundColor: "#7dc383",
           backgroundImage:
@@ -20,55 +20,56 @@ export const SelectPuzzle = () => {
           backgroundRepeat: "repeat",
         }}
       >
-        <div>
+        <div className="row-span-1">
           <SelectPuzzlePiece
             text="Select Your Puzzle"
-            className1="md:min-w-[900px] md:min-h-[550px] min-w-[400px] min-h-[600px]"
-            className="relative max-w-"
-          />
-        </div>
-        <div className="flex flex-col justify-center items-center">
-          <div>
-          <SelectPuzzlePiece
-            text="Default Puzzles:"
-            className1="md:min-w-[200px] md:min-h-[150px] min-w-[150px] min-h-[50px]"
-            className="absolute z-10 top-[100px] md:left-[300px] left-[30px]"
-          />
-          </div>
-          <div className="flex flex-row  md:gap-15 gap-4 justify-center items-center absolute top-[200px] z-10 ">
+            className="relative w-[65vw] max-w-[70vw] h-[70vh]"
+          >
+           <div className="flex flex-col gap-15 justify-center items-center puzzles">
+          <div className="mb-[30px]">
+          <h1 className="text-[1.5em] font-bold text-white  z-10">
+            Default Puzzles:
+          </h1>
+         
+          <div className="flex flex-row  md:gap-15 gap-4 justify-center items-center relative z-10 ">
             {defaultImages.map((puzzle, i) => {
               return (
                 <PuzzleImgPrev
                   key={i}
                   src={puzzle}
-                  className1="md:max-w-[110px] md:max-h-[110px]  max-w-[80px] ring-2 ring-black/20"
+                  className1="w-[clamp(64px,8vw,150px)]
+  aspect-square ring-2 ring-black/20"
                   className2="rounded-2xl  bg-[#DCCE99] flex justify-center items-center"
                   onClick={() => handleClick(puzzle)}
                 />
               );
             })}
+          </div> 
           </div>
           <div>
-          <SelectPuzzlePiece
-            text="Uploaded Puzzles:"
-            className1="md:min-w-[200px] md:min-h-[150px] min-w-[200px] min-h-[50px]"
-            className="absolute z-10 md:bottom-[170px] bottom-[250px] md:left-[300px] left-[20px]"
-          />
-          </div>
-          <div className="flex flex-row  md:gap-15 gap-4 justify-center items-center absolute z-10 bottom-[110px]">
+          <h1 className="text-[1.5em] font-bold text-white  z-10">
+            Uploaded Puzzles:
+          </h1>
+          
+          <div className="flex flex-row  md:gap-15 gap-4 justify-center items-center relative z-10 ">
             {defaultImages.map((puzzle, i) => {
               return (
                 <PuzzleImgPrev
                   key={i}
                   src={puzzle}
-                  className1="md:max-w-[110px] md:max-h-[110px] max-w-[80px] ring-2 ring-black/20 "
+                  className1="w-[clamp(64px,8vw,150px)]
+  aspect-square ring-2 ring-black/20"
                   className2="rounded-2xl  bg-[#DCCE99]  flex justify-center items-center"
                   onClick={() => handleClick(puzzle)}
                 />
               );
             })}
           </div>
+          </div>
+        </div> 
+          </SelectPuzzlePiece>
         </div>
+        
       </div>
     </div>
   );
