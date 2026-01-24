d92568eb7906b2375c1c804f6eae63f05a2530e1
Zaara-Fatima <bano6096@gmail.com>
2026-01-24 16:46:58 +0530

    Implement drag-and-drop in TileTray component
    Added drag-and-drop functionality to TileTray component.

--- src/components/game/TileTray.jsx
+++ src/components/game/TileTray.jsx
@@ -1,16 +1,36 @@
+import { useDrop } from "react-dnd";
 import PuzzleTile from "./PuzzleTile";
 
-export default function TileTray({ availableTiles, imageSrc, gridSize }) {
+export default function TileTray({ availableTiles, imageSrc, gridSize, setAvailableTiles, setBoard }) {
+  const [,dropRef]=useDrop({
+    accept:'tile',
+    drop:(item)=>{
+      const tileIndex= item.tileIndex;
+
+      setBoard((prev)=>
+        prev.map((cell)=>cell===tileIndex?null:cell)
+      );
+
+      setAvailableTiles((prev)=>prev.includes(tileIndex)?prev:[...prev, tileIndex])
+    }
+  })
+
   return (
-    <div className="grid grid-cols-[repeat(4,minmax(60px,1fr))] w-full h-full  gap-2 p-4 overflow-y-auto ">
-      {availableTiles.map(tileIndex => (
-        <PuzzleTile
-          key={tileIndex}
-          index={tileIndex}
-          imageSrc={imageSrc}
-          gridSize={gridSize}
-        />
-      ))}
+    <div ref={dropRef} className="grid grid-cols-4 gap-4 place-items-center overflow-y-auto">
+  {availableTiles.map((tileIndex) => (
+    <div
+      key={tileIndex}
+      className="aspect-square w-[50px] md:w-[70px] lg:w-[80px]"
+    >
+      <PuzzleTile
+        index={tileIndex}
+        imageSrc={imageSrc}
+        gridSize={gridSize}
+      />
     </div>
+  ))}
+</div>
   );
 }
+
+// grid grid-cols-[repeat(4,minmax(60px,1fr))] w-full h-full  gap-2 p-4 overflow-y-auto 
